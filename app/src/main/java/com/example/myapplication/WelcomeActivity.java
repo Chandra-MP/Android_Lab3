2955f349f03eef59121de324123a8285993c76f3
Chandra-MP <ishupandey17@gmail.com>
2023-07-07 22:13:36 +0530

    Added some links

--- app/src/main/java/com/example/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/myapplication/WelcomeActivity.java
@@ -3,9 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -31,6 +34,40 @@ public class WelcomeActivity extends AppCompatActivity {
         TextView txtViewPhone = findViewById(R.id.textViewPhone);
         txtViewPhone.setText("Your Phone is: " + phone);
 
+
+        ImageView instagramBtn = findViewById(R.id.imageView7);
+        ImageView gitBtn = findViewById(R.id.imageView6);
+        ImageView linkBtn = findViewById(R.id.imageView8);
+
+        instagramBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.instagram.com/idk_nd_idc_2/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        gitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlGit = "https://github.com/Chandra-MP";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlGit));
+                startActivity(intent);
+            }
+        });
+
+        linkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlLink = "https://www.linkedin.com/in/chandra-mani-pandey-ba994513a/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlLink));
+                startActivity(intent);
+            }
+        });
+
+
+
         //Exit Button
         Button exit = findViewById(R.id.button2);
         exit.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +76,7 @@ public class WelcomeActivity extends AppCompatActivity {
              finish();
             }
         });
+
+
     }
 }
